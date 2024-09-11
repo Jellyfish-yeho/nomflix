5dacd54cf12508787697341a440e56eba4370fd8
unknown <kurer456@naver.com>
2024-09-11 21:47:50 +0900

    animation4: drag options

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { duration } from "moment";
+import { useRef } from "react";
 /* 
   framer motion
   https://www.framer.com/docs/animation
@@ -34,8 +34,10 @@ import { duration } from "moment";
   * 색상값들은 rgba 나 hex같은 numeric value를 넣어야 애니메이션이 발생한다. (점진적 변화)
 
   - drag: 요소 드래그 활성화. x, y 로 수평/수직 방향 제한 가능
-  - dragConstraints: 드래그 범위 제한. top/bottom/left/right 범위 지정 가능. 값을 모두 0으로 하면 중앙으로 되돌아가진다!
-
+  - dragConstraints: 드래그 범위 제한. top/bottom/left/right 범위 지정 가능. 값을 모두 0으로 하면 중앙으로 되돌아가진다
+  특정 요소 안에서만 가능하게 하려면 해당 요소에 ref를 등록 후 이를 dragConstraint로 지정하면 됨.
+  - dragSnapToOrigin: dragConstraints에서 값을 0으로 한것과 같은 효과. 중앙으로 되돌아감.
+  - dragElastic: 드래그에 저항하는 힘. 0 과 1사이의 값.
 */
 
 const Wrapper = styled.div`
@@ -55,12 +57,12 @@ const BiggerBox = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    overflow: hidden;
+    /* overflow: hidden; */
 `;
 
 const Box = styled(motion.div)`
-    width: 200px;
-    height: 200px;
+    width: 100px;
+    height: 100px;
     background-color: rgba(255, 255, 255, 1);
     border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
@@ -72,12 +74,15 @@ const boxVariants = {
 };
 
 function App() {
+    const biggerBoxRef = useRef<HTMLDivElement>(null);
     return (
         <Wrapper>
-            <BiggerBox>
+            <BiggerBox ref={biggerBoxRef}>
                 <Box
                     drag
-                    dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }}
+                    dragSnapToOrigin
+                    dragElastic={0.5}
+                    dragConstraints={biggerBoxRef}
                     variants={boxVariants}
                     whileHover="hover"
                     whileTap="click"
