543dd601a9a72276ea871799cf6279dd68848ec6
unknown <kurer456@naver.com>
2024-09-11 22:47:03 +0900

    animation6 : svg transform

--- src/App.tsx
+++ src/App.tsx
@@ -59,10 +59,15 @@ import { useEffect, useRef } from "react";
   scrollX/Y= scrolled pixel amount
   scrollX/YProgress= 0 ~ 1 사이 % 값
 
+  - svg animation
+  svg > path : fill, stroke, strokeWidth...
+
+
+
 */
 
 const Wrapper = styled(motion.div)`
-    height: 200vh;
+    height: 100vh;
     width: 100vw;
     display: flex;
     justify-content: center;
@@ -70,37 +75,37 @@ const Wrapper = styled(motion.div)`
     background: linear-gradient(135deg, rgb(137, 234, 203), rgb(255, 178, 91));
 `;
 
-const Box = styled(motion.div)`
-    width: 100px;
-    height: 100px;
-    background-color: rgba(255, 255, 255, 1);
-    border-radius: 40px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Svg = styled.svg`
+    width: 200px;
+    height: 200px;
+    path {
+        stroke: white;
+        stroke-width: 2;
+    }
 `;
 
-function App() {
-    const x = useMotionValue(0);
-    // const scale = useTransform(x, [-200, 0, 200], [2, 1, 0.1]);
-    const rotateZ = useTransform(x, [-200, 200], [-360, 360]);
-    const gradient = useTransform(
-        x,
-        [-200, 200],
-        [
-            "linear-gradient(135deg, rgb(234, 137, 194), rgb(165, 68, 249))",
-            "linear-gradient(135deg, rgb(234, 232, 137), rgb(134, 181, 255))",
-        ]
-    );
-    const { scrollY, scrollYProgress } = useScroll();
-    const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-    useEffect(() => {
-        scrollY.on("change", function () {
-            console.log(scrollY.get(), scrollYProgress.get());
-        });
-    }, [scrollY, scrollYProgress]);
+const svg = {
+    start: { fill: "rgba(255,255,255,0)", pathLength: 0 },
+    end: {
+        fill: "rgba(255,255,255,1)",
+        pathLength: 1,
+        transition: {
+            duration: 3,
+        },
+    },
+};
 
+function App() {
     return (
-        <Wrapper style={{ background: gradient }}>
-            <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
+        <Wrapper>
+            <Svg xmlns="http://www.w3.org/2000/Svg" viewBox="0 0 448 512">
+                <motion.path
+                    variants={svg}
+                    initial="start"
+                    animate="end"
+                    d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z"
+                />
+            </Svg>
         </Wrapper>
     );
 }
