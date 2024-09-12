09540780b25031185b0dabb413cb0baf2ce28669
unknown <kurer456@naver.com>
2024-09-12 23:29:19 +0900

    slider 2

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,17 @@ import { useState } from "react";
 
   Slider 만들기
   - next를 누르면 -> 1번:exit실행, 2번 animate 실행
+  - 숫자 배열을 i개 만들어서 map 하고 box에 animate를 주었는데...
+  -> visible state의 값을 변경하고, 이 값을 Box 의 key로 사용하면 
+    react.js는 key가 변경되면서 기존 element가 사라지고 새롭게 생겼다고 생각함 ㄷㄷㄷ
+    즉 exit animation이 실행된다는 것. 
+  - custom property: variants에 데이터를 보낼 수 있음 
+    custom에 값을 보내면, animatePresence에도 같은 custom값을 보낸 후,
+    varient를 object를 리턴하는 함수로 변경
+    const box = {
+      entry: (back: boolean) => ({ x: back ? -500 : 500, opacity: 0, scale: 0 }), 
+    }
+  - mode="wait" : exit 을 한 후에 animation 실행
 */
 
 const Wrapper = styled(motion.div)`
@@ -44,31 +55,40 @@ const Box = styled(motion.div)`
 `;
 
 const box = {
-    invisible: { x: 500, opacity: 0, scale: 0 },
-    visible: { x: 0, opacity: 1, scale: 1, transition: { duration: 1 } },
-    exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 1 } },
+    entry: (back: boolean) => ({ x: back ? -500 : 500, opacity: 0, scale: 0 }),
+    center: { x: 0, opacity: 1, scale: 1, transition: { duration: 1 } },
+    exit: (back: boolean) => ({
+        x: back ? 500 : -500,
+        opacity: 0,
+        scale: 0,
+        transition: { duration: 1 },
+    }),
 };
 
 function App() {
     const [visible, setVisible] = useState(1);
-    const nextPlease = () => setVisible((prev) => (prev === 6 ? 6 : prev + 1));
-    const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+    const [back, setBack] = useState(false);
+    const nextPlease = () => {
+        setBack(false);
+        setVisible((prev) => (prev === 6 ? 6 : prev + 1));
+    };
+    const prevPlease = () => {
+        setBack(true);
+        setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+    };
     return (
         <Wrapper>
-            <AnimatePresence>
-                {[1, 2, 3, 4, 5, 6].map((i) =>
-                    i === visible ? (
-                        <Box
-                            variants={box}
-                            initial="invisible"
-                            animate="visible"
-                            exit="exit"
-                            key={i}
-                        >
-                            {i}
-                        </Box>
-                    ) : null
-                )}
+            <AnimatePresence mode="wait" custom={back}>
+                <Box
+                    custom={back}
+                    variants={box}
+                    initial="entry"
+                    animate="center"
+                    exit="exit"
+                    key={visible}
+                >
+                    {visible}
+                </Box>
             </AnimatePresence>
             <button onClick={prevPlease}>prev</button>
             <button onClick={nextPlease}>next</button>
