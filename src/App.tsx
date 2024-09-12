081ec96b257ea435f6fea02272af084c3c47ff65
unknown <kurer456@naver.com>
2024-09-12 23:49:12 +0900

    layoutId

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 /*
-  layout prop : 이 속성을 준 element의 layout이 바뀔 때, 자동으로 animate 된다 ㄷㄷㄷ
+  layout : 이 속성을 준 element의 layout이 바뀔 때, 자동으로 animate 된다 ㄷㄷㄷ
   - framerMotion이 css 변화를 감지
+  
+  layoutId : 같은 id를 가진 요소를 같은 component로 인식하여 animation을 준다
+
 */
 
 const Wrapper = styled(motion.div)`
     height: 100vh;
     width: 100vw;
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
-    flex-direction: column;
     background: linear-gradient(135deg, rgb(137, 234, 203), rgb(255, 178, 91));
 `;
 
@@ -42,13 +44,18 @@ function App() {
     const toggleClicked = () => setClicked((prev) => !prev);
     return (
         <Wrapper onClick={toggleClicked}>
-            <Box
-                style={{
-                    justifyContent: clicked ? "center" : "flex-start",
-                    alignItems: clicked ? "center" : "flex-start",
-                }}
-            >
-                <Circle layout />
+            <Box>
+                {!clicked ? (
+                    <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+                ) : null}
+            </Box>
+            <Box>
+                {clicked ? (
+                    <Circle
+                        layoutId="circle"
+                        style={{ borderRadius: 0, scale: 2 }}
+                    />
+                ) : null}
             </Box>
         </Wrapper>
     );
